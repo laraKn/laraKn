68d9a90d24af82577aeab1a40a8affb81aa40404
laraKn <laraKn@users.noreply.github.com>
2016-04-25 17:30:57 +0200

    Added files via upload

---
+++ equi_triangle.py
@@ -0,0 +1,12 @@
+import turtle
+turtle.home()
+
+edge = 100
+
+turtle.forward(edge)
+turtle.right(120)
+turtle.forward(edge)
+turtle.right(120)
+turtle.forward(edge)
+
+input("press enter to exit")
---
+++ rectangle.py
@@ -0,0 +1,15 @@
+import turtle
+turtle.home()
+
+width = 200
+height = 100
+
+turtle.forward(width)
+turtle.left(90)
+turtle.forward(height)
+turtle.left(90)
+turtle.forward(width)
+turtle.left(90)
+turtle.forward(height)
+
+input ("press enter to exit")
